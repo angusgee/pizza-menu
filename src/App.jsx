5324eabd69c72a7e0cc83feb86dc492775d8a79e
MaltaWebDev <MaltaWebDev@gmail.com>
2023-10-03 07:04:38 +0200

    Add nested Pizza component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Pizza />
+    </>
+  );
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>;
 }
 
 export default App;
